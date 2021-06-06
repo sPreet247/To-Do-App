b140558731abf58cde729d6e15956959c3d0d3f1
Preet Singh <66023067+sPreet247@users.noreply.github.com>
2021-06-06 10:36:37 -0400

    folderStructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles.css';
-import Main from 'components/Main';
+import Header from 'components/Header';
+import Add from 'components/Add';
+import List from 'components/List';
 function App() {
   return (
     <div className="App">
       <h1>#ToDo</h1>
-      <Main />
+      <Header />
+      <Add />
+      <List />
     </div>
   );
 }
